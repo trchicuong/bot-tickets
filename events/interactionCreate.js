b80561f77434338a49b149384aebc813239cd0a0
hoangphats1 <71956878+hoangphats1@users.noreply.github.com>
2025-04-10 11:53:24 +0700

    Update timezone saveticket

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -183,26 +183,27 @@ module.exports = async (client, int) => {
             const channel = int.guild.channels.cache.get(int.channelId);
 
             await channel.messages.fetch().then(async msg => {
-                let messages = msg.filter(msg => msg.author.bot !== true).map(m => {
-                    const date = new Date(m.createdTimestamp).toLocaleString();
-                    const user = `${m.author.tag}${m.author.id === int.customId.split('_')[1] ? ' (ticket creator)' : ''}`;
-
-                    return `${date} - ${user} : ${m.attachments.size > 0 ? m.attachments.first().proxyURL : m.content}`;
-                }).reverse().join('\n');
-
+                let messages = msg
+                    .filter(msg => msg.author.bot !== true)
+                    .map(m => {
+                        const date = new Date(m.createdTimestamp).toLocaleString('vi-VN', { timeZone: 'Asia/Ho_Chi_Minh' });
+                        const user = `${m.author.tag}${m.author.id === int.customId.split('_')[1] ? ' (ticket creator)' : ''}`;
+        
+                        return `${date} - ${user} : ${m.attachments.size > 0 ? m.attachments.first().proxyURL : m.content}`;
+                    }).reverse().join('\n');
+        
                 if (messages.length < 1) messages = 'There are no messages in this ticket... strange';
-
+        
                 const ticketID = Date.now();
-
+        
                 const stream = await createWriteStream(`./data/${ticketID}.txt`);
-
+        
                 stream.once('open', () => {
                     stream.write(`User ticket ${int.customId.split('_')[1]} (channel #${channel.name})\n\n`);
-                    stream.write(`${messages}\n\nLogs ${new Date(ticketID).toLocaleString()}`);
-
+                    stream.write(`${messages}\n\nLogs ${new Date(ticketID).toLocaleString('vi-VN', { timeZone: 'Asia/Ho_Chi_Minh' })}`);
                     stream.end();
                 });
-
+        
                 stream.on('finish', () => int.reply({ files: [`./data/${ticketID}.txt`] }));
             });
         }
